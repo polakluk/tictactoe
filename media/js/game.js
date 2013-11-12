99f2b9b9718c823d52f76eb2459ff27b43588a40
unknown <3lf@.(none)>
2013-11-11 22:20:10 -0500

    moves reflect on the game board using websocket

--- media/js/game.js
+++ media/js/game.js
@@ -10,6 +10,13 @@ Game.Init = function() {
 	Game.elements.desk = $( 'table.table-desk', Game.elements.main_container );
 	Game.elements.td_team = $( 'tr#trTeam td:eq(1)' );
 	Game.elements.td_turn = $( 'tr#trTurn td:eq(1)' );
+	
+	Game.id = Game.elements.desk.attr( 'data-game' );
+	// init Pusher
+	Game.Pusher = new Pusher( pusher_key );
+	Game.Pusher.connection.bind( 'connected', function() {
+		Game.socket = Game.Pusher.connection.socket_id;
+	});
 }
 
 // this method determines, whether the game object should be used for this page
@@ -24,6 +31,7 @@ Game.BindEvents = function() {
 		var post_data = {
 			'row' : obj.attr( 'data-row' ),
 			'col' : obj.attr( 'data-col' ),
+			'socket' : Game.socket,
 			'turn' : Game.elements.td_turn.attr( 'data-turn' )
 					};
 		
@@ -36,8 +44,10 @@ Game.BindEvents = function() {
 			})
 			.fail( function() { alert( 'Something went terribly wrong :( ' ); });
 	});	
+	Game.MapPusher();
 }
 
+// what happens after player user clicks on a field
 Game.FieldClick = function( data ) {
 
 	switch( data.state ) {
@@ -48,7 +58,6 @@ Game.FieldClick = function( data ) {
 		case 1: // we're good to go so just mark the spot
 			{
 				Game.MarkField( data.row, data.col, data.game, data.team );
-				Game.UpdateStats( data.turn, data.team );
 				break;
 			}
 		case 2: // game is over
@@ -66,9 +75,10 @@ Game.FieldClick = function( data ) {
 	Game.elements.panel_msg.prepend( data.html );
 }	
 
+// update game statistics
 Game.UpdateStats = function( turn, team ) {
 	var obj = null;
-	if( turn == 1 ) {
+	if( turn == 2 ) {
 		obj = $( '<span />', { 'class' : 'label label-danger', 'text' : 'Red' } );
 	} else {
 		obj = $( '<span />', { 'class' : 'label label-primary', 'text' : 'Blue' } );
@@ -78,6 +88,7 @@ Game.UpdateStats = function( turn, team ) {
 	Game.elements.td_turn.attr( 'data-turn', turn );
 }
 
+// mark winning fields
 Game.MarkWinner = function( row, col, d, game, num ) {
 	var dir = [ // row, col, dir
 			[ 1, 0 ], //vertical line
@@ -93,6 +104,7 @@ Game.MarkWinner = function( row, col, d, game, num ) {
 	}
 }
 
+// mark the field
 Game.MarkField = function( row, col, game, team) {
 	var obj;
 	if( team == 1 ){
@@ -105,8 +117,26 @@ Game.MarkField = function( row, col, game, team) {
 	obj_td.removeClass( 'empty' );
 }
 
+
+// find the right field on the board
 Game.FieldTd = function( row, col, game ) {
 	return $( 'table[data-game="'+game+'"] td[data-col="'+col+'"][data-row="'+row+'"]' );
+}
+
+// maps events from Pusher
+Game.MapPusher = function() {
+	Game.channels = {};
+	Game.channels.main = Game.Pusher.subscribe( 'game-' + Game.id );
+	
+	// change game stats
+	Game.channels.main.bind( 'update_stats', function( data ) {
+		Game.UpdateStats( data.turn, data.team );
+	});
+	
+	// mark move
+	Game.channels.main.bind( 'move', function( data ) {
+		Game.MarkField( data.row, data.col, data.game, data.team );		
+	});
 }
 
 $(function(){
